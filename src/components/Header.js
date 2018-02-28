9d4a843685463161a9b0302e491d65cc839c3655
alan345 <alan345@gmail.com>
2018-02-28 15:12:40 -0800

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,26 +10,7 @@ class Header extends Component {
     return (
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            new
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/top" className="ml1 no-underline black">
-            top
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/search" className="ml1 no-underline black">
-            search
-          </Link>
-          {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
-                submit
-              </Link>
-            </div>
-          )}
+        YEAH
         </div>
         <div className="flex flex-fixed">
           {authToken ? (
