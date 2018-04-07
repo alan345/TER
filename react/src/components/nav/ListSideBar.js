15273b6fa3be7c631c151b9ab911a3262830a3de
alan345 <alan345@gmail.com>
2018-04-07 12:35:23 -0700

    login in sidebar

--- react/src/components/nav/ListSideBar.js
+++ react/src/components/nav/ListSideBar.js
@@ -4,49 +4,9 @@ import {Link} from 'react-router-dom'
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
 import List from 'material-ui/List'
 import Icon from 'material-ui/Icon'
+import { AUTH_TOKEN } from '../../constants/constants'
 
-  const mailFolderListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <Icon>arrow_back</Icon>
-      </ListItemIcon>
-    </ListItem>
 
-  <Link to='/' className='link'>
-    <ListItem button>
-      <ListItemIcon>
-        <Icon>view_quilt</Icon>
-      </ListItemIcon>
-      <ListItemText primary='Blog' />
-    </ListItem>
-    </Link>
-    <Link to='/drafts' className='link'>
-      <ListItem button>
-        <ListItemIcon>
-          <Icon>mode_edit</Icon>
-        </ListItemIcon>
-        <ListItemText primary='Drafts' />
-      </ListItem>
-    </Link>
-    <Link to='/cars' className='link'>
-    <ListItem button>
-      <ListItemIcon>
-        <Icon>directions_car</Icon>
-      </ListItemIcon>
-      <ListItemText primary='Cars' />
-    </ListItem>
-    </Link>
-    <Link to='/users' className='link'>
-    <ListItem button>
-      <ListItemIcon>
-        <Icon>group</Icon>
-      </ListItemIcon>
-      <ListItemText primary='Users' />
-    </ListItem>
-    </Link>
-  </div>
-)
 
 
 class ListSideBar extends Component {
@@ -67,9 +27,76 @@ class ListSideBar extends Component {
 
 
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN)
     return (
       <div>
-        <List>{mailFolderListItems}</List>
+        <List>
+
+          <div>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>arrow_back</Icon>
+              </ListItemIcon>
+            </ListItem>
+
+          <Link to='/' className='link'>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>view_quilt</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Blog' />
+            </ListItem>
+            </Link>
+            <Link to='/drafts' className='link'>
+              <ListItem button>
+                <ListItemIcon>
+                  <Icon>mode_edit</Icon>
+                </ListItemIcon>
+                <ListItemText primary='Drafts' />
+              </ListItem>
+            </Link>
+            <Link to='/cars' className='link'>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>directions_car</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Cars' />
+            </ListItem>
+            </Link>
+            <Link to='/users' className='link'>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>group</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Users' />
+            </ListItem>
+            </Link>
+
+            {!authToken && (
+            <Link to='/login' className='link'>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>account_circle</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Login' />
+            </ListItem>
+            </Link>
+          )}
+
+            {authToken && (
+              <ListItem button onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                this.props.history.replace(`/`)
+              }}>
+                <ListItemIcon>
+                  <Icon>exit_to_app</Icon>
+                </ListItemIcon>
+                <ListItemText primary='logout' />
+              </ListItem>
+            )}
+
+          </div>
+        </List>
       </div>
   )
   }
