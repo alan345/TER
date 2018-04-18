69dcb4afae7ca312694eb0b97ca27e51e2511cd2
alan345 <alan345@gmail.com>
2018-04-17 18:19:34 -0700

    autocomplete

--- react/src/components/post/Autocomplete.js
+++ react/src/components/post/Autocomplete.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import TextField from 'material-ui/TextField'
-
+import CarsPageList from '../car/CarsPageList'
 
 
 class Autocomplete extends Component {
   state = {
-    searchAutocomplete : ''
+    query : ''
   }
-  onChangeAutocomplete(e) {
-    console.log(e.target.value )
-    this.setState({ searchAutocomplete: e.target.value })
-  }
+
   render() {
     return (
       <div>
         <TextField
           value={this.state.searchAutocomplete}
-          onChange={e => this.onChangeAutocomplete(e)}
+          onChange={e => this.setState({query:e.target.value})}
           type='text'
           label='Search'
         />
+      
+        <CarsPageList
+          showTitle={false}
+          showMore={false}
+          query={this.state.query}
+          orderBy={this.state.orderBy}/>
       </div>
     )
   }
