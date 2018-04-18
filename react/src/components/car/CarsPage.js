b4b99812dca48912c7f801b7960e43e4778c8bb8
alan345 <alan345@gmail.com>
2018-04-17 18:10:34 -0700

    list car

--- react/src/components/car/CarsPage.js
+++ react/src/components/car/CarsPage.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router'
+import React, {Component} from 'react'
+import {withRouter} from 'react-router'
 import CarsPageList from './CarsPageList'
 import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import Icon from 'material-ui/Icon'
+
 
 class CarsPage extends Component {
-  // state = {
-  //   carsQueryConnection:''
-  // }
-
+  state = {
+    query: '',
+    orderBy: 'name_ASC'
+  }
 
   render() {
-    return (
-      <React.Fragment>
-        <div className='paperOut'>
-          <Paper className='paperIn'>
-        <Button onClick={() => this.props.history.push('/car/create')} variant='raised' color='primary'>
-          + Create Car
-        </Button>
-        <CarsPageList/>
+    return (<React.Fragment>
+      <div className='paperOut'>
+        <Paper className='paperIn'>
+          <TextField
+            onChange={e => this.setState({query: e.target.value})}
+            type='text'
+            label='Search'
+            />
+          {' '}
+          <Button onClick={() => this.props.history.push('/car/create')} variant='raised' color='primary'>
+            + Create Car
+          </Button>
+          <div onClick={() => {
+              this.setState({
+                orderBy: this.state.orderBy === 'name_ASC'
+                  ? 'name_DESC'
+                  : 'name_ASC'
+              })
+            }}>
+            {
+              this.state.orderBy === 'name_ASC'
+                ? (<Icon>keyboard_arrow_down</Icon>)
+                : (<Icon>keyboard_arrow_up</Icon>)
+            }
+          </div>
+
+        <CarsPageList query={this.state.query} orderBy={this.state.orderBy}/>
         </Paper>
-    </div>
-  </React.Fragment>
-    )
+      </div>
+    </React.Fragment>)
   }
 }
 
--- react/src/components/car/CarsPageList.js
+++ react/src/components/car/CarsPageList.js
@@ -7,11 +7,6 @@ import Icon from 'material-ui/Icon'
 import NotAuth from '../nav/NotAuth'
 
 class CarsPage extends React.Component {
-  state = {
-    query: '',
-  }
-
-
 
   render() {
     const {carsQueryConnection} = this.props
@@ -25,47 +20,16 @@ class CarsPage extends React.Component {
       return null
     }
     const {edges, aggregate} = this.props.carsQueryConnection.carsConnection
-    const {orderBy} = this.props.carsQueryConnection.variables
-
     if (carsQueryConnection.loading) {
       return (<div className='flex w-100 h-100 items-center justify-center pt7'>
         <div>Loading (from {process.env.REACT_APP_GRAPHQL_ENDPOINT})</div>
       </div>)
     }
-
     return (
+
       <div>
             <div className='flex justify-between items-center'>
               <h1>Cars ({edges.length}/{aggregate.count})</h1>
-              <div>
-                <input type='text' autoFocus='autoFocus' onFocus={function(e) {
-                    var val = e.target.value
-                    e.target.value = ''
-                    e.target.value = val
-                  }} className='w-100 pa2 mv2 br2 b--black-20 bw1' onChange={e => {
-                    this.setState({query: e.target.value})
-                    carsQueryConnection.refetch({
-                      where: {
-                        name_contains: e.target.value
-                      }
-                    })
-                  }} placeholder='Search' value={this.state.query}/>
-              </div>
-
-              <div onClick={() => {
-                  carsQueryConnection.refetch({
-                    orderBy: orderBy === 'name_ASC'
-                      ? 'name_DESC'
-                      : 'name_ASC'
-                  })
-                }}>
-                {
-                  orderBy === 'name_ASC'
-                    ? (<Icon>keyboard_arrow_down</Icon>)
-                    : (<Icon>keyboard_arrow_up</Icon>)
-                }
-              </div>
-
 
             </div>
             {edges && edges.map(car =>
@@ -73,7 +37,7 @@ class CarsPage extends React.Component {
                   <Car
                     key={car.node.id}
                     car={car.node}
-                    refresh={() => carsQueryConnection.refetch()}
+
                     isCar={!car.node.isPublished}/>
                   ))
                 }
@@ -141,11 +105,13 @@ const DRAFTS_QUERY = gql `
 
 export default graphql(DRAFTS_QUERY, {
   name: 'carsQueryConnection',
-  options: {
+  options: props => ({
     fetchPolicy: 'network-only',
     variables: {
-      orderBy: 'createdAt_ASC'
+      orderBy: props.orderBy,
+      where: {
+        name_contains: props.query
+      }
     }
-
-  }
+  })
 })(CarsPage)
