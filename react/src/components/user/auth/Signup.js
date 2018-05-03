15e3403812aa5900667612fc26ac0f65746eb47d
alan345 <alan345@gmail.com>
2018-05-03 11:00:25 -0700

    signup

--- react/src/components/user/auth/Signup.js
+++ react/src/components/user/auth/Signup.js
@@ -7,14 +7,40 @@ import Paper from 'material-ui/Paper'
 import Button from 'material-ui/Button'
 import TextField from 'material-ui/TextField'
 
+import { InputAdornment } from 'material-ui/Input'
+import Icon from 'material-ui/Icon'
+import { LinearProgress } from 'material-ui/Progress'
+
 
 class Signup extends Component {
   state = {
     email: '',
     password: '',
     name: '',
+    activeStep: 0,
+    maxStep: 3,
   }
 
+  handleNext = () => {
+    this.setState({
+      activeStep: this.state.activeStep + 1,
+    })
+    if(this.state.activeStep === 2) {
+      this._confirm()
+    }
+  };
+
+  handleKey = (data) => {
+    if(data.charCode === 13) { //keyPress enter
+      this.handleNext()
+    }
+  }
+
+  handleBack = () => {
+    this.setState({
+      activeStep: this.state.activeStep - 1,
+    })
+  };
 
   render() {
     return (
@@ -25,34 +51,75 @@ class Signup extends Component {
         </h4>
         <div className='flex flex-column'>
 
+          <LinearProgress variant='buffer' value={this.state.activeStep * 100 / this.state.maxStep } valueBuffer={0.1} />
+
+
+
+          <br/>
+            <div className='tac'>
+              <TextField
+                value={this.state.name}
+                onChange={e => this.setState({ name: e.target.value })}
+                type='text'
+                className={'wrapperAnimate ' + (this.state.activeStep === 0 ? 'focusField' : 'notFocusField')}
+                label='Your name'
+                onKeyPress={this.handleKey}
+                InputProps={{
+                  endAdornment: (
+                  <InputAdornment position='end'>
+                    {this.state.activeStep === 0 && (
+                      <Button onClick={this.handleNext} variant='fab' color='primary' mini>
+                        <Icon>navigate_next</Icon>
+                      </Button>
+                    )}
+                  </InputAdornment>
+                )}}
+              />
+            <br/><br/>
+            {this.state.activeStep >= 1 && (
+              <TextField
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+                type='text'
+                className={'wrapperAnimate ' + (this.state.activeStep === 1 ? 'focusField' : 'notFocusField')}
+                label='Your email address'
+                InputProps={{
+                  endAdornment: (
+                  <InputAdornment position='end'>
+                    {this.state.activeStep === 1 && (
+                      <Button onClick={this.handleNext} variant='fab' color='primary' mini>
+                        <Icon>navigate_next</Icon>
+                      </Button>
+                    )}
+                  </InputAdornment>
+                )}}
+                />
+
+            )}
+            <br/><br/>
+            {this.state.activeStep >= 2 && (
             <TextField
-              value={this.state.name}
-              onChange={e => this.setState({ name: e.target.value })}
-              type='text'
-              label='Your name'
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+              className={'wrapperAnimate ' + (this.state.activeStep === 2 ? 'focusField' : 'notFocusField')}
+              type='password'
+              label='Choose a safe password'
+              InputProps={{
+                endAdornment: (
+                <InputAdornment position='end'>
+                  {this.state.activeStep === 2 && (
+                    <Button onClick={this.handleNext} variant='fab' color='primary' mini>
+                      <Icon>done</Icon>
+                    </Button>
+                  )}
+                </InputAdornment>
+              )}}
             />
-
-          <TextField
-            value={this.state.email}
-            onChange={e => this.setState({ email: e.target.value })}
-            type='text'
-            label='Your email address'
-          />
-
-
-          <TextField
-            value={this.state.password}
-            onChange={e => this.setState({ password: e.target.value })}
-            type='password'
-            label='Choose a safe password'
-          />
+            )}
+          </div>
         </div>
-        <div className='flex mt3'>
-          <Button variant='raised' onClick={() => this._confirm()}>
-            Ok
-          </Button>
 
-        </div>
+
         <SnackBarCustom ref={instance => { this.child = instance }}/>
       </Paper>
       </div>
