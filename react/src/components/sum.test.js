63b356d86bae13c7b5e1142df552e2683475d374
alan345 <alan345@gmail.com>
2018-05-08 14:35:16 -0700

    removed JEST

--- react/src/components/sum.test.js
+++ react/src/components/sum.test.js
@@ -1,3 +1,12 @@
+// 
+// const sum = require('./sum');
+//
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+
+
 import React from 'react'
 import CarsPageList from './car/CarsPageList'
 import renderer from 'react-test-renderer'
