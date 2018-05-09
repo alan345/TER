1e2439638d357d69366ab5a1ae273dff85d9e285
alan345 <alan345@gmail.com>
2018-05-09 15:02:30 -0700

    test not working

--- react/src/components/sum.test.js
+++ react/src/components/sum.test.js
@@ -1,4 +1,4 @@
-// 
+//
 // const sum = require('./sum');
 //
 // test('adds 1 + 2 to equal 3', () => {
@@ -7,13 +7,37 @@
 
 
 
+import { configure, shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
-import CarsPageList from './car/CarsPageList'
-import renderer from 'react-test-renderer'
+
+import Signup from './user/auth/Signup'
+
+
+configure({ adapter: new Adapter() })
+
 
 test('Renders Cars', () => {
-  const component = renderer.create(
-    <CarsPageList/>
+  const component = mount(
+    <Signup/>
   )
   expect(component).toMatchSnapshot()
 })
+
+
+
+//
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import Signup from './user/auth/Signup'
+//
+// it('Renders the SimpleInput', () => {
+//
+//   const component = mount(
+//     <Signup
+//
+//     />
+//   );
+//
+//   expect(component).toMatchSnapshot();
+// });
