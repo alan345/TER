c691dc0c21d27ecb865ad76c3cad3ee803763281
alan345 <alan345@gmail.com>
2018-05-15 14:00:31 -0700

    login

--- react/src/components/nav/TopHello.js
+++ react/src/components/nav/TopHello.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { AUTH_TOKEN } from '../../constants/constants'
 import { Link } from 'react-router-dom'
 import { graphql, compose } from 'react-apollo'
@@ -6,38 +6,65 @@ import gql from 'graphql-tag'
 import MenuAvatar from '../nav/MenuAvatar'
 
 
-function TopHello(props) {
+class TopHello extends Component {
+  // state = {
+  //   userToken: {
+  //     id: ''
+  //   }
+  // }
+
+  componentDidMount(){
+    // const authToken = localStorage.getItem(AUTH_TOKEN)
+    // if(authToken) {
+    //   // console.log(authToken)
+    //   // console.log(this.props.me)
+    //   this.props.me.refetch()
+    // }
+  }
+  componentWillReceiveProps(nextProps){
+    console.log(nextProps)
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+    if(authToken) {
+      // console.log(authToken)
+      // console.log(this.props.me)
+      nextProps.me.refetch()
+    }
+    // console.log(authToken)
+  }
+  render() {
     const authToken = localStorage.getItem(AUTH_TOKEN)
     const userToken = JSON.parse(localStorage.getItem('userToken'))
-    return (
-      <div>
-      {authToken ? (
+    // console.log(this.props.me.me)
+      return (
         <div>
-          {props.userQuery.user && (
-            <MenuAvatar user={props.userQuery.user} nameFile={props.userQuery.user.nameFile}/>
-          )}
+        {authToken ? (
+          <div>
+            {this.props.me.me && (
+              <MenuAvatar user={this.props.me.me} nameFile={this.props.me.me.nameFile}/>
+            )}
+          </div>
+        ) : (
+          <div>
+            {userToken && (
+              <div>
+              Hi {userToken.name}!
+              </div>
+            )}
+          </div>
+        )}
+        {!authToken && (
+          <Link to='/login' className='ml1 no-underline black'>
+            login
+          </Link>
+        )}
         </div>
-      ) : (
-        <div>
-          {userToken && (
-            <div>
-            Hi {userToken.name}!
-            </div>
-          )}
-        </div>
-      )}
-      {!authToken && (
-        <Link to='/login' className='ml1 no-underline black'>
-          login
-        </Link>
-      )}
-      </div>
-    )
+      )
+    }
   }
 
   const USER_QUERY = gql`
-    query UserQuery($where: UserWhereUniqueInput!) {
-      user(where: $where) {
+    query Me {
+      me {
         id
         email
         role
@@ -52,14 +79,5 @@ if(!userToken) {
   userToken = {id: ''}
 }
 export default compose(
-  graphql(USER_QUERY, {
-    name: 'userQuery',
-    options: props => ({
-      variables: {
-          where: {
-            id: userToken.id
-          }
-        },
-    }),
-  })
-  )(TopHello)
+  graphql(USER_QUERY, {name: 'me'})
+)(TopHello)
