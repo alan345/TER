7fa041a387bfbbf32be5ea31ad14e04f43a4c29b
alan345 <alan345@gmail.com>
2018-05-29 15:24:25 +0100

    ok express cleaner

--- express/app.js
+++ express/app.js
@@ -16,8 +16,8 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
