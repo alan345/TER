4c515cb2cc6beeb3739ce365a1a956e75477de40
alan345 <alan345@gmail.com>
2018-05-29 04:13:06 -0700

    resend email validation component

--- react/src/components/nav/EmailValidated.js
+++ react/src/components/nav/EmailValidated.js
@@ -4,9 +4,8 @@ import Paper from '@material-ui/core/Paper'
 import Icon from '@material-ui/core/Icon'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
-import Button from '@material-ui/core/Button'
-import SnackBarCustom from './SnackBarCustom'
 import Loading from './Loading'
+import ResendEmailValidation from './ResendEmailValidation'
 
 
 class EmailValidated extends Component {
@@ -24,52 +23,13 @@ class EmailValidated extends Component {
         <div className='paperOut'>
           <Paper className='paperIn'>
             <Icon>error_outline</Icon>{' '}
-              Email not validated. Link sent by email, or {' '}
-              {this.state.interval ? (
-                <span>wait {this.state.interval}s to resend.</span>
-              ) : (
-                <Button variant='raised' onClick={() => this.sendEmail()}>
-                  Resend
-                </Button>
-              )}
-              <SnackBarCustom ref={instance => { this.child = instance }}/>
+              Email not validated.<ResendEmailValidation />
             </Paper>
           </div>
         )
     } else {
       return(<div></div>)
     }
-  }
-  sendEmail = async () => {
-    this.startTimer()
-    await this.props.sendLinkValidateEmailMutation({
-      variables: {
-      },
-    })
-    .then((result) => {
-      const messageSnackBar = `Email sent successfully to ${result.data.sendLinkValidateEmail.email}!`
-      this.child._openSnackBar(messageSnackBar)
-
-    })
-    .catch((e) => {
-      this.child._openSnackBar(e.graphQLErrors[0].message)
-    })
-  }
-
-  startTimer = () => {
-    this.initTimer()
-    let intervalId = setInterval(this.timer, 1000)
-    this.setState({ intervalId })
-  };
-  timer = () => {
-    if (this.state.interval > 0) {
-      this.setState({ interval: this.state.interval -1 })
-    } else {
-      clearTimeout(this.state.intervalId)
-    }
-  }
-  initTimer() {
-    this.setState({ interval: 40 })
   }
 }
 
@@ -83,15 +43,7 @@ const USER_QUERY = gql`
   }
 `
 
-const SEND_LINK_VALIDATE_EMAIL_MUTATION = gql`
-  mutation sendLinkValidateEmailMutation {
-    sendLinkValidateEmail {
-      email
-    }
-  }
-`
 
 export default compose(
   graphql(USER_QUERY, {name: 'me'}),
-  graphql(SEND_LINK_VALIDATE_EMAIL_MUTATION, { name: 'sendLinkValidateEmailMutation' }),
 )(EmailValidated)
---
+++ react/src/components/nav/ResendEmailValidation.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
+import Button from '@material-ui/core/Button'
+import SnackBarCustom from './SnackBarCustom'
+
+class ResendEmailValidation extends Component {
+  state = {
+    interval : 0
+  }
+  render() {
+
+    return (
+      <span>
+        {this.state.interval ? (
+          <span>wait {this.state.interval}s to resend.</span>
+        ) : (
+          <Button variant='raised' onClick={() => this.sendEmail()}>
+            Resend Email
+          </Button>
+        )}
+        <SnackBarCustom ref={instance => { this.child = instance }}/>
+      </span>
+    )
+  }
+  sendEmail = async () => {
+    this.startTimer()
+    await this.props.sendLinkValidateEmailMutation({
+      variables: {
+      },
+    })
+    .then((result) => {
+      const messageSnackBar = `Email sent successfully to ${result.data.sendLinkValidateEmail.email}!`
+      this.child._openSnackBar(messageSnackBar)
+
+    })
+    .catch((e) => {
+      this.child._openSnackBar(e.graphQLErrors[0].message)
+    })
+  }
+
+  startTimer = () => {
+    this.initTimer()
+    let intervalId = setInterval(this.timer, 1000)
+    this.setState({ intervalId })
+  };
+  timer = () => {
+    if (this.state.interval > 0) {
+      this.setState({ interval: this.state.interval -1 })
+    } else {
+      clearTimeout(this.state.intervalId)
+    }
+  }
+  initTimer() {
+    this.setState({ interval: 40 })
+  }
+}
+
+
+
+const SEND_LINK_VALIDATE_EMAIL_MUTATION = gql`
+  mutation sendLinkValidateEmailMutation {
+    sendLinkValidateEmail {
+      email
+    }
+  }
+`
+
+export default compose(
+  graphql(SEND_LINK_VALIDATE_EMAIL_MUTATION, { name: 'sendLinkValidateEmailMutation' }),
+)(ResendEmailValidation)
