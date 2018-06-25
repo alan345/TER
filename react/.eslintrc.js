0ca7ded51a69daad195fb93bcc8461d5fc2d7768
alan345 <alan345@gmail.com>
2018-06-25 11:30:27 -0700

    ok

---
+++ react/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "rules": {
+        // enable additional rules
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/prop-types": "off",
+        "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
+        "padded-blocks": ["error", "never"],
+        "semi": ["error", "never"]
+    },
+    "plugins": [
+      "react"
+    ],
+    "parserOptions": {
+      "ecmaVersion": 7,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "globals": {
+      "localStorage": true
+    }
+
+}
