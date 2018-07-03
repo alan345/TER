e3822fe1a31a38384075939e38f7e6cf103af0ae
alan345 <alan345@gmail.com>
2018-07-03 16:17:56 -0700

    menu avatar much simplier

--- react/src/components/nav/layout/MenuAvatar.js
+++ react/src/components/nav/layout/MenuAvatar.js
@@ -4,23 +4,19 @@ import Button from '@material-ui/core/Button'
 import ImageTemplate from '../ImageTemplate'
 import MenuItem from '@material-ui/core/MenuItem'
 import {AUTH_TOKEN} from '../../../constants/constants'
-import {Manager, Target, Popper} from 'react-popper'
-import Paper from '@material-ui/core/Paper'
-import ClickAwayListener from '@material-ui/core/ClickAwayListener'
-import Grow from '@material-ui/core/Grow'
-import MenuList from '@material-ui/core/MenuList'
+import Menu from '@material-ui/core/Menu'
 
 class MenuAvatar extends Component {
   state = {
     anchorEl: null
-  };
-  handleToggle = () => {
-    this.setState({
-      open: !this.state.open
-    })
-  };
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
 
   handleClose = (page) => {
+    this.setState({ anchorEl: null })
     if (page === 'profile') {
       this.props.history.push('/user/' + this.props.user.id)
     }
@@ -28,44 +24,31 @@ class MenuAvatar extends Component {
       localStorage.removeItem(AUTH_TOKEN)
       this.props.history.replace(`/login`)
     }
-
     this.setState({open: false})
-  };
+  }
 
   render() {
-    const {open} = this.state
-    return (<div>
-      <Manager>
-        <Target>
-          <div ref={node => {
-              this.target1 = node
-            }}>
-            <Button aria-owns={open
-                ? 'menu-list-grow'
-                : null} aria-haspopup='true' onClick={this.handleToggle}>
-              <ImageTemplate format={'avatar'} nameFile={this.props.nameFile}/>
-            </Button>
-          </div>
-        </Target>
-        <Popper placement='bottom-start' eventsEnabled={open}>
-          <ClickAwayListener onClickAway={this.handleClose}>
-            <Grow in={open} id='menu-list-grow' style={{
-                transformOrigin: '0 0 0'
-              }}>
-              <Paper>
-                <MenuList role='menu'>
-
-                  <MenuItem onClick={() => this.handleClose('profile')}>Profile</MenuItem>
-                  <MenuItem onClick={() => this.handleClose('logout')}>Logout</MenuItem>
-                </MenuList>
-              </Paper>
-            </Grow>
-          </ClickAwayListener>
-        </Popper>
-      </Manager>
-    </div>
-  )
-  }
+    const { anchorEl } = this.state
+       return (
+         <div>
+           <Button
+             aria-owns={anchorEl ? 'simple-menu' : null}
+             aria-haspopup="true"
+             onClick={this.handleClick}>
+             <ImageTemplate format={'avatar'} nameFile={this.props.nameFile} />
+           </Button>
+           <Menu
+             id="simple-menu"
+             anchorEl={anchorEl}
+             open={Boolean(anchorEl)}
+             onClose={this.handleClose}
+           >
+            <MenuItem onClick={() => this.handleClose('profile')}>Profile</MenuItem>
+            <MenuItem onClick={() => this.handleClose('logout')}>Logout</MenuItem>
+           </Menu>
+         </div>
+       )
+    }
 }
 
 export default withRouter(MenuAvatar)
