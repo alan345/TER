ab192c65b22e5c6e8d42937175e2f5278d79b9af
Alan <alan345@gmail.com>
2024-07-19 10:56:11 -0700

    ok

--- client/src/user/User.type.tsx
+++
@@ -1,5 +0,0 @@
-import type { AppRouter } from "../../../server";
-import type { inferRouterOutputs } from "@trpc/server";
-type RouterOutputs = inferRouterOutputs<AppRouter>;
-
-export type Worker = RouterOutputs["createWorker"];
