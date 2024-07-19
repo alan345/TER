408b80ebf7aa53f07033ef4091335f57b7f9a50e
Alan <alan345@gmail.com>
2024-07-18 21:19:18 -0700

    bette rlogin

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -1,32 +1,50 @@
 import React from "react";
 import { trpc } from "../utils/trpc";
-// import { Button } from "@headlessui/react";
 import { AppContext } from "../ContextProvider";
+import { LoginMutation } from "./LoginMutation";
 
 export function Login() {
-  const context = React.useContext(AppContext);
-  const createWorkerMutation = trpc.login.useMutation({});
-  const handleCreateWorker = () => {
-    createWorkerMutation.mutate(
-      { login: "", password: "" },
-      {
-        onSuccess: () => context.updateUserId(),
-      }
-    );
-  };
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [login, setLogin] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
     <div>
-      <button
-        disabled={createWorkerMutation.isPending}
-        onClick={handleCreateWorker}
-        className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[hover]:data-[active]:bg-sky-700"
-      >
-        {createWorkerMutation.isPending ? "Closing..." : "Login"}
-      </button>{" "}
-      {createWorkerMutation.error && (
-        <p className="text-red-600">
-          Something went wrong! {createWorkerMutation.error.message}
-        </p>
+      {!isOpen ? (
+        <button
+          className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[hover]:data-[active]:bg-sky-700"
+          onClick={() => setIsOpen(true)}
+        >
+          Login
+        </button>
+      ) : (
+        <div>
+          <div>
+            <input
+              value={login}
+              className="text-black"
+              type="text"
+              placeholder="Login"
+              onChange={(e) => setLogin(e.target.value)}
+            />
+          </div>
+          <div className="mt-1">
+            <input
+              value={password}
+              className="text-black"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mt-1">
+            <LoginMutation
+              onCancel={() => setIsOpen(false)}
+              login={login}
+              password={password}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
---
+++ client/src/auth/LoginMutation.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { trpc } from "../utils/trpc";
+import { AppContext } from "../ContextProvider";
+
+type Props = {
+  password: string;
+  login: string;
+  onCancel: () => void;
+};
+export function LoginMutation(props: Props) {
+  const context = React.useContext(AppContext);
+  const createWorkerMutation = trpc.login.useMutation({});
+  const handleCreateWorker = () => {
+    createWorkerMutation.mutate(
+      { login: "", password: "" },
+      {
+        onSuccess: () => context.updateUserId(),
+      }
+    );
+  };
+  return (
+    <div>
+      <button
+        disabled={createWorkerMutation.isPending}
+        onClick={handleCreateWorker}
+        className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[hover]:data-[active]:bg-sky-700"
+      >
+        {createWorkerMutation.isPending ? "Closing..." : "Login"}
+      </button>{" "}
+      <button onClick={props.onCancel}>Cancel</button>
+      {createWorkerMutation.error && (
+        <p className="text-red-600">
+          Something went wrong! {createWorkerMutation.error.message}
+        </p>
+      )}
+    </div>
+  );
+}
