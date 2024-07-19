1e7825fd17785e8e88ecaff008b7e09719b83502
Alan <alan345@gmail.com>
2024-07-19 11:01:35 -0700

    ter

--- client/src/auth/AuthManagementParent.tsx
+++ client/src/auth/AuthManagementParent.tsx
@@ -2,7 +2,7 @@ import { AuthManagement } from "./AuthManagement";
 
 export function AuthManagementParent() {
   return (
-    <div className="min-h-10 mb-10">
+    <div className="min-h-16 mb-10">
       <AuthManagement />
     </div>
   );
