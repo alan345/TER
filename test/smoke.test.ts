0595d7dca0b1cbd1393d9041b1c328a1b788b189
Alan <alan345@gmail.com>
2024-07-19 16:40:48 -0700

    test

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -6,6 +6,7 @@ test("go to /", async ({ page }) => {
   await page.goto("/");
 
   // check for the page
+  await page.waitForSelector("img");
   await page.waitForSelector(`text=Trpc Express React`);
   await page.waitForSelector(`text=Loading...`);
   await page.waitForSelector(`text=Login`);
@@ -15,7 +16,6 @@ test("go to /", async ({ page }) => {
   await page.waitForSelector(`text=Last Name`);
   await page.waitForSelector(`text=Email`);
   await page.waitForSelector(`text=Avatar`);
-  await page.waitForSelector("img");
 
   // Login experience
   await page.locator("#login-button").click();
