0b96d0c92990934a2fda8d896c5a6e35056a23ef
Alan <alan345@gmail.com>
2024-07-19 13:45:48 -0700

    link

--- client/src/template/BackgroundPage.tsx
+++ client/src/template/BackgroundPage.tsx
@@ -8,8 +8,14 @@ const BackgroundPage = (props: Props) => {
     <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 ">
       <header className="text-center flex justify-center items-center p-10">
         <div>
-          <img src={levelpath} alt="logo" className="object-center m-2 h-24" />
-          <b>T</b>rpc <b>E</b>xpress <b>R</b>eact
+          <a href="https://github.com/alan345/TER" target="_blank">
+            <img
+              src={levelpath}
+              alt="logo"
+              className="object-center m-2 h-24"
+            />
+            <b>T</b>rpc <b>E</b>xpress <b>R</b>eact
+          </a>
         </div>
       </header>
       <div className="mt-10 mx-10 pb-10">{props.children}</div>
