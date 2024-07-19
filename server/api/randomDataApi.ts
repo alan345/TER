7bc1bb6f0242b84a13308b7e40fe3ff6c1d8dbe3
Alan <alan345@gmail.com>
2024-07-19 11:26:13 -0700

    ok

--- server/api/randomDataApi.ts
+++ server/api/randomDataApi.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { User } from "../type/User.type";
 
-// Todo: Add logic to get the correct uri based on the environment
 const uri = "https://random-data-api.com/api/v2/users";
 
 export const randomDataApi = {
@@ -16,7 +15,6 @@ export const randomDataApi = {
       return res.data;
     } catch (error: any) {
       if (axios.isAxiosError(error) && error && error.response) {
-        // Todo: We should not pass the error given by an external API, but create a custom message to have a better experience
         throw error.response.data.error_message;
       } else {
         throw new Error("Something went wrong!");
