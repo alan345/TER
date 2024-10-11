4d1c43b9af26b41e6542c7abfabfb8bd25935130
Alan <alan345@gmail.com>
2024-10-11 09:33:23 -0700

    logoTer

--- client/src/template/layout/LogoTer.tsx
+++ client/src/template/layout/LogoTer.tsx
@@ -1,11 +1,11 @@
-import levelpath from "../images/ter-logo.png"
+import logoTer from "../images/ter-logo.png"
 
 export const LogoTer = () => {
   return (
     <div className="p-4 flex items-center justify-center h-24 ">
       <div>
         <a href="https://github.com/alan345/TER" target="_blank" rel="noopener noreferrer">
-          <img src={levelpath} alt="logo" className="w-24" />
+          <img src={logoTer} alt="logo" className="w-24" />
           <b>T</b>rpc <b>E</b>xpress <b>R</b>eact
         </a>
       </div>
