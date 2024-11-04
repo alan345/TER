751eab95e27ebe11c87b99d76f2160e8c6adbfa0
Alan <alan345@gmail.com>
2024-11-04 15:59:19 -0800

    better approach workspace

--- server/router/userRouter.ts
+++ server/router/userRouter.ts
@@ -1,6 +1,6 @@
 import { protectedProcedure, router } from "../trpc"
 import { z } from "zod"
-import { usersTable } from "../../drizzle/src/db/schema"
+import { usersTable } from "@ter/drizzle/src/db/schema"
 import { eq } from "drizzle-orm"
 
 export const userRouter = router({
