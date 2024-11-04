75e5fb64042a55c39869693548a528cc0aead15c
Alan <alan345@gmail.com>
2024-11-04 13:08:34 -0800

    easiest password

--- drizzle/src/seed/initUsersData.ts
+++ drizzle/src/seed/initUsersData.ts
@@ -11,14 +11,14 @@ export const initUsersData = [
     name: "Paul Doe",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat3&accessoriesType=Prescription01&hatColor=Red&hairColor=Red&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Black&eyeType=Hearts&eyebrowType=UpDown&mouthType=ScreamOpen&skinColor=Yellow",
-    password: "$2b$10$4AjaxVnYZnw0ZB55ggLm1.tbWqVc2mjecpYnDuYORn8QQzFhcSHgW", // hashed version. non-hashed is "verySecurePassword"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "john@example.com",
     name: "John Doe",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairFrizzle&accessoriesType=Prescription01&hairColor=Black&facialHairType=Blank&facialHairColor=Brown&clotheType=BlazerSweater&clotheColor=Gray02&eyeType=Side&eyebrowType=Default&mouthType=Default&skinColor=Black",
-    password: "$2b$10$.w4jBul1DjE37JSuQdQBJuw44AA5SwDfhv/uh11OmioOSuqBgNEWe", // hashed version. non-hashed is "veryVerySecurePassword"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
 
   {
@@ -26,140 +26,140 @@ export const initUsersData = [
     name: "User Fifty",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads02&hairColor=SilverGray&facialHairType=BeardMagestic&facialHairColor=BlondeGold&clotheType=ShirtCrewNeck&clotheColor=Red&eyeType=Close&eyebrowType=SadConcernedNatural&mouthType=Grimace&skinColor=DarkBrown",
-    password: "$2b$10$ABCDE12345FGHIJKLMNOPQRSTUVWXYZ67890", // Example hashed password
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "lisa@example.com",
     name: "Lisa Smith",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Sunglasses&hairColor=Blonde&facialHairType=Blank&clotheType=CollarSweater&eyeType=WinkWacky&eyebrowType=RaisedExcitedNatural&mouthType=Grimace&skinColor=Pale",
-    password: "$2b$10$dhEjiLMN7P5k5p.ZPvFWh.R4u3DzIznT9zEfGhReNTtYPVE9MW77i", // hashed version. non-hashed is "randomPassword1"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "mike@example.com",
     name: "Mike Johnson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads01&accessoriesType=Kurt&hairColor=Brown&facialHairType=BeardLight&clotheType=BlazerShirt&eyeType=WinkWacky&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Tanned",
-    password: "$2b$10$yT.JbJeYNLnmy5jK9Es0UOhRKaWOUtmJ1GRX0dviuNmhlSx9B9U0W", // hashed version. non-hashed is "randomPassword2"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "jane@example.com",
     name: "Jane Brown",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Round&hairColor=Auburn&facialHairType=Blank&clotheType=Hoodie&eyeType=Wink&eyebrowType=UnibrowNatural&mouthType=Smile&skinColor=Brown",
-    password: "$2b$10$LzFKo5t7KvNb7F1sQ2W.O.W2dCefrZnD5RG9hTfX3EqLyNMbZ2E9e", // hashed version. non-hashed is "randomPassword3"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "emma@example.com",
     name: "Emma White",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairSides&accessoriesType=Sunglasses&hairColor=Black&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=AngryNatural&mouthType=Grimace&skinColor=DarkBrown",
-    password: "$2b$10$MBFbXjFXeg.q.TzDCQ7NXu1DyoOJtAErUpFxnOY5f0f6FqUpfFbVG", // hashed version. non-hashed is "randomPassword4"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "james@example.com",
     name: "James Green",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat4&accessoriesType=Round&hairColor=Brown&facialHairType=MoustacheFancy&facialHairColor=Red&clotheType=Overall&eyeType=Hearts&eyebrowType=FlatNatural&mouthType=Serious&skinColor=Tanned",
-    password: "$2b$10$k8Tw0H9ck5NG0gjYu9UJvOrFtZGiHs2MRM6qisSxsifzZL6LTTBrS", // hashed version. non-hashed is "randomPassword5"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "charlotte@example.com",
     name: "Charlotte Lee",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat2&accessoriesType=Wayfarers&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Close&eyebrowType=FlatNatural&mouthType=Disbelief&skinColor=Black",
-    password: "$2b$10$cRwx4kPphVrXtRvlkDCnGurnNUU.SrFMFwT7fjxVcz9CKRxYq8AiG", // hashed version. non-hashed is "randomPassword6"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "david@example.com",
     name: "David Harris",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat3&accessoriesType=Kurt&hairColor=Brown&facialHairType=MoustacheMagnum&facialHairColor=Red&clotheType=CollarSweater&eyeType=Hearts&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Pale",
-    password: "$2b$10$JmOtZHHjX.RvsARfjxfVTO0ptUKVZ79OBH.K8D5iGVtjo8zt0rX9O", // hashed version. non-hashed is "randomPassword7"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "mia@example.com",
     name: "Mia Clark",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Round&hairColor=Red&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Wink&eyebrowType=Default&mouthType=Smile&skinColor=Light",
-    password: "$2b$10$MxRJ5OFxZ4/5lkXa6gJIQum5PzMHjciEvb9dNrOdhLJ7hz5bbXtCK", // hashed version. non-hashed is "randomPassword8"
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "user1@example.com",
     name: "User One",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairCurly&hairColor=Blonde&facialHairType=MoustacheFancy&facialHairColor=Auburn&clotheType=GraphicShirt&clotheColor=PastelYellow&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown",
-    password: "$2b$10$ABCDEFHIJKLMNOPQRSTUVWXYZ1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "user2@example.com",
     name: "User Two",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=Hat&hatColor=Blue03&clotheType=BlazerSweater&eyeType=Squint&eyebrowType=AngryNatural&mouthType=Smile&skinColor=Pale",
-    password: "$2b$10$ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "liam@example.com",
     name: "Liam Smith",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortRound&accessoriesType=Prescription02&hairColor=Black&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=PastelBlue&eyeType=Happy&eyebrowType=Default&mouthType=Twinkle&skinColor=Pale",
-    password: "$2b$10$BCDEFGHIJKLMNOPQRSTUVWXYZA1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "olivia@example.com",
     name: "Olivia Brown",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairCurvy&accessoriesType=Kurt&hairColor=Red&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=PastelGreen&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Tanned",
-    password: "$2b$10$CDEFGHIJKLMNOPQRSTUVWXYZAB1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "noah@example.com",
     name: "Noah Wilson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairTheCaesar&accessoriesType=Wayfarers&hairColor=Blonde&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=ShirtScoopNeck&clotheColor=Blue03&eyeType=Wink&eyebrowType=FlatNatural&mouthType=Default&skinColor=Light",
-    password: "$2b$10$DEFGHIJKLMNOPQRSTUVWXYZABC1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "ava@example.com",
     name: "Ava Taylor",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Round&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Red&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Pale",
-    password: "$2b$10$EFGHIJKLMNOPQRSTUVWXYZABCD1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "ethan@example.com",
     name: "Ethan Davis",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=Brown&facialHairType=BeardMedium&facialHairColor=Brown&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light",
-    password: "$2b$10$FGHIJKLMNOPQRSTUVWXYZABCDE1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "sophia@example.com",
     name: "Sophia Anderson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairBob&accessoriesType=Prescription01&hairColor=Black&facialHairType=Blank&clotheType=Overall&clotheColor=Blue03&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Tanned",
-    password: "$2b$10$GHIJKLMNOPQRSTUVWXYZABCDEF1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "mason@example.com",
     name: "Mason Thomas",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads01&accessoriesType=Prescription02&hairColor=Auburn&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=White&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=DarkBrown",
-    password: "$2b$10$HIJKLMNOPQRSTUVWXYZABCDEFG1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "isabella@example.com",
     name: "Isabella Jackson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairMiaWallace&accessoriesType=Kurt&hairColor=Red&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Pale",
-    password: "$2b$10$IJKLMNOPQRSTUVWXYZABCDEFGH1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "william@example.com",
     name: "William White",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortWaved&accessoriesType=Blank&hairColor=Black&facialHairType=BeardLight&facialHairColor=Black&clotheType=CollarSweater&clotheColor=Gray01&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
-    password: "$2b$10$JKLMNOPQRSTUVWXYZABCDEFGHI1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
 
   {
@@ -167,48 +167,48 @@ export const initUsersData = [
     name: "James Martin",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=BeardMagestic&facialHairColor=Auburn&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=Side&eyebrowType=RaisedExcited&mouthType=Default&skinColor=Pale",
-    password: "$2b$10$LMNOPQRSTUVWXYZABCDEFGHIJK1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "charlotte_t@example.com",
     name: "Charlotte Thompson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairCurly&accessoriesType=Round&hairColor=Black&facialHairType=Blank&clotheType=Overall&clotheColor=Gray01&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light",
-    password: "$2b$10$MNOPQRSTUVWXYZABCDEFGHIJKL1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "benjamin@example.com",
     name: "Benjamin Garcia",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairTheCaesarSidePart&accessoriesType=Sunglasses&hairColor=Brown&facialHairType=BeardMedium&facialHairColor=Brown&clotheType=BlazerSweater&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
-    password: "$2b$10$NOPQRSTUVWXYZABCDEFGHIJKLM1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "amelia@example.com",
     name: "Amelia Martinez",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairNotTooLong&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=PastelGreen&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light",
-    password: "$2b$10$OPQRSTUVWXYZABCDEFGHIJKLMN1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "elijah@example.com",
     name: "Elijah Robinson",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=Black&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Blue03&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=DarkBrown",
-    password: "$2b$10$PQRSTUVWXYZABCDEFGHIJKLMNO1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "harper@example.com",
     name: "Harper Clark",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Kurt&hairColor=Red&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=White&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Pale",
-    password: "$2b$10$QRSTUVWXYZABCDEFGHIJKLMNOP1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
   {
     email: "lucas@example.com",
     name: "Lucas Rodriguez",
     image:
       "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortWaved&accessoriesType=Prescription01&hairColor=Brown&facialHairType=BeardMedium&facialHairColor=Brown&clotheType=CollarSweater&clotheColor=Blue03&eyeType=Side&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light",
-    password: "$2b$10$RSTUVWXYZABCDEFGHIJKLMNOPQ1234567890",
+    password: "$2b$10$6K4nDgAfAfE5ktaWbHU3meBaZYbsWMBm97/8628/hM7jTz3gJUyMm", // hashed version. non-hashed is "securePassword"
   },
 ]
