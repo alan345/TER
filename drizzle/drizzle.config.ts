234d051d263481163553b6e3b86fdc571e4c3735
Alan <alan345@gmail.com>
2024-11-15 15:20:19 -0800

    better

--- drizzle/drizzle.config.ts
+++ drizzle/drizzle.config.ts
@@ -2,13 +2,11 @@ import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 import { config } from "dotenv"
 config({ path: "../.env" })
-const databaseUrl = process.env.DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL!
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: {
-    url: databaseUrl!,
-  },
+  dbCredentials: { url: databaseUrl },
 })
--- drizzle/src/seed/seed.ts
+++ drizzle/src/seed/seed.ts
@@ -4,10 +4,10 @@ import { drizzle } from "drizzle-orm/node-postgres"
 import { usersTable } from "../db/schema"
 import { config } from "dotenv"
 config({ path: "../.env" })
-const databaseUrl = process.env.DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL!
 
 async function main() {
-  const db = drizzle(databaseUrl!)
+  const db = drizzle(databaseUrl)
   await db.delete(usersTable)
   let data = await db.insert(usersTable).values(initUsersData)
   console.log("Getting all users from the database: ", data)
