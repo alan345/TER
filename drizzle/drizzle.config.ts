53d8b5e764c813f28e2d9179b297db90928a5227
Alan <alan345@gmail.com>
2024-11-15 15:18:23 -0800

    better seed

--- drizzle/drizzle.config.ts
+++ drizzle/drizzle.config.ts
@@ -1,13 +1,14 @@
 import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
-import { DATABASE_URL } from "@ter/shared/env"
-// console.log("DATABASE_URL", process.env.DATABASE_URL)
+import { config } from "dotenv"
+config({ path: "../.env" })
+const databaseUrl = process.env.DATABASE_URL
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    // url: process.env.DATABASE_URL!,
-    url: DATABASE_URL,
+    url: databaseUrl!,
   },
 })
--- drizzle/src/seed/seed.ts
+++ drizzle/src/seed/seed.ts
@@ -2,35 +2,15 @@ import "dotenv/config"
 import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { usersTable } from "../db/schema"
-import { DATABASE_URL } from "@ter/shared/env"
-async function main() {
-  // const db = drizzle(process.env.DATABASE_URL!)
-  console.log(DATABASE_URL)
-  const db = drizzle(DATABASE_URL)
+import { config } from "dotenv"
+config({ path: "../.env" })
+const databaseUrl = process.env.DATABASE_URL
 
+async function main() {
+  const db = drizzle(databaseUrl!)
   await db.delete(usersTable)
-  // .where(eq(usersTable.email, user.email))
   let data = await db.insert(usersTable).values(initUsersData)
-  // console.log("New user created!")
-  // const users = await db.select().from(usersTable)
   console.log("Getting all users from the database: ", data)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  // await db
-  //   .update(usersTable)
-  //   .set({
-  //     name: "henry",
-  //   })
-  //   .where(eq(usersTable.email, user.email))
-  // console.log("User info updated!")
-  // await db.delete(usersTable).where(eq(usersTable.email, user.email))
-  // console.log("User deleted!")
 }
 console.log("main")
 main()
