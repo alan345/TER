88d482d6ab270184a1d3e3e9afbf13e68eb7ae5c
Alan <alan345@gmail.com>
2024-11-16 13:49:09 -0800

    better env acces

--- packages/drizzle/drizzle.config.ts
+++ packages/drizzle/drizzle.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 import { config } from "dotenv"
-config({ path: "../.env" })
+config({ path: "../../.env" })
 const databaseUrl = process.env.DATABASE_URL!
 
 export default defineConfig({
--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -3,7 +3,7 @@ import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { usersTable } from "../db/schema"
 import { config } from "dotenv"
-config({ path: "../.env" })
+config({ path: "../../.env" })
 const databaseUrl = process.env.DATABASE_URL!
 
 async function main() {
