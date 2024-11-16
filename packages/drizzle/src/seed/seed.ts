33e89131aefd3a56d1fe81bce11277967303e42f
Alan <alan345@gmail.com>
2024-11-16 13:53:49 -0800

    better seed

--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -7,10 +7,11 @@ config({ path: "../../.env" })
 const databaseUrl = process.env.DATABASE_URL!
 
 async function main() {
+  console.log(`Seeding ${databaseUrl}...`)
   const db = drizzle(databaseUrl)
   await db.delete(usersTable)
   let data = await db.insert(usersTable).values(initUsersData)
-  console.log("Getting all users from the database: ", data)
+  console.log(`Done! ${data.rowCount} rows inserted`)
+  process.exit(0)
 }
-console.log("main")
 main()
