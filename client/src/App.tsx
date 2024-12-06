57c34fef37384bc0944a2ed57c8772663e967800
Alan <alan345@gmail.com>
2024-12-06 10:09:24 -0800

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,6 @@ const App = () => {
             return fetch(url, {
               ...options,
               credentials: "include",
-              referrerPolicy: "no-referrer",
             })
           },
         }),
