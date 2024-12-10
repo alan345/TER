d2c0398f1d3326a3118c390764ca90161145978a
Alan <alan345@gmail.com>
2024-12-09 20:18:41 -0800

    devices

--- client/src/components/auth/ProfilePage.tsx
+++ client/src/components/auth/ProfilePage.tsx
@@ -3,6 +3,8 @@ import { AppContext } from "../../ContextProvider"
 import ProfileQuery from "./ProfileQuery"
 import SessionData from "./SessionData"
 import Logout from "./Logout"
+import { Link } from "react-router-dom"
+import { Devices } from "@phosphor-icons/react"
 
 const ProfilePage = () => {
   const context = React.useContext(AppContext)
@@ -18,6 +20,15 @@ const ProfilePage = () => {
       </div>
       <ProfileQuery meId={context.me.id} />
       <SessionData />
+      <div className="mt-6">
+        <Link className="link" to={`/devices?userId=${context.me.id}`}>
+          <button className="btn-white">
+            <div className="flex items-center">
+              <Devices className="text-2xl mr-2" /> Devices
+            </div>
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
