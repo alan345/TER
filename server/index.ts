57ad0495d2e7a246ff41c8ced36fca8799810e5f
Alan <alan345@gmail.com>
2025-01-13 17:10:34 -0800

    seperate create context in a file

--- server/index.ts
+++ server/index.ts
@@ -9,52 +9,14 @@ import { deviceRouter } from "./router/deviceRouter"
 import { healthRouter } from "./router/healthRouter"
 import { beerRouter } from "./router/beerRouter"
 import { t } from "./trpc"
-import { drizzle } from "drizzle-orm/node-postgres"
-import { usersTable } from "@ter/drizzle"
-import * as schema from "@ter/drizzle"
-import { eq } from "drizzle-orm"
-import { cookieNameAuth, cookieNameDeviceIds } from "./configTer"
 import { config } from "dotenv"
-import manageDevice from "./helper/manageDevice"
+import { createContext } from "./createContext"
 config({ path: "../.env" })
-
-const secretJwt = process.env.JWT_SECRET
-const databaseUrl = process.env.DATABASE_URL
 
 export interface UserIDJwtPayload extends jwt.JwtPayload {
   id: string
   exp: number
   iat: number
-}
-
-export const createContext = async ({ req, res }: trpcExpress.CreateExpressContextOptions) => {
-  if (!secretJwt) throw new Error("JWT_SECRET is not defined")
-  if (!databaseUrl) throw new Error("DATABASE_URL is not defined")
-  const config = { secretJwt, databaseUrl }
-  const cookies = req.cookies
-  const authToken = cookies[cookieNameAuth]
-  const db = drizzle(databaseUrl, { schema })
-
-  if (authToken) {
-    try {
-      let decoded = jwt.verify(authToken, secretJwt) as UserIDJwtPayload
-      if (decoded) {
-        const user = await db.query.usersTable.findFirst({ where: eq(usersTable.id, decoded.id) })
-        if (!user) throw new Error("User not found")
-
-        const cookies = req.cookies
-        const deviceIdsFromCookieString: string = cookies[cookieNameDeviceIds]
-        const device = await manageDevice.getDeviceFromCookieString(db, user.id, deviceIdsFromCookieString)
-
-        if (!device) throw new Error("Device not found")
-        return { req, res, user, db, config, decoded, device }
-      }
-    } catch (error) {
-      console.log("error", error)
-      res.clearCookie(cookieNameAuth)
-    }
-  }
-  return { req, res, db, config }
 }
 
 export const mergeRouters = t.mergeRouters
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,21 +1,19 @@
-import { initTRPC, TRPCError } from "@trpc/server";
-export const t = initTRPC.context<Context>().create();
-import { createContext } from "./index";
+import { initTRPC, TRPCError } from "@trpc/server"
+import { createContext } from "./createContext"
+export const t = initTRPC.context<Context>().create()
 
-type Context = Awaited<ReturnType<typeof createContext>>;
+type Context = Awaited<ReturnType<typeof createContext>>
 
-export const publicProcedure = t.procedure;
-export const router = t.router;
+export const publicProcedure = t.procedure
+export const router = t.router
 
-export const protectedProcedure = t.procedure.use(async function isAuthed(
-  opts
-) {
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
   if (!opts.ctx.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
+    throw new TRPCError({ code: "UNAUTHORIZED" })
   }
   return opts.next({
     ctx: {
       user: opts.ctx.user,
     },
-  });
-});
+  })
+})
