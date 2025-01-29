bb866f5d6df0bd50699d44e8e86d594ea56f1252
Alan <alan345@gmail.com>
2025-01-28 21:22:15 -0800

    upgrade

--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -2,6 +2,7 @@ import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { usersTable, devicesTable } from "../db/schema"
 import dotenv from "dotenv"
+import { eq } from "drizzle-orm"
 dotenv.config({ path: "../../server.env" })
 const databaseUrl = process.env.DATABASE_URL!
 
@@ -11,6 +12,7 @@ async function main() {
   await db.delete(usersTable)
   await db.delete(devicesTable)
   let data = await db.insert(usersTable).values(initUsersData)
+
   console.log(`Done! ${data.rowCount} rows inserted`)
   process.exit(0)
 }
