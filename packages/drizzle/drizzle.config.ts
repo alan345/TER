36b33d7ba926ef1121e14799aa410f5b8ab030c2
Alan <alan345@gmail.com>
2025-01-29 14:06:53 -0800

    better

--- packages/drizzle/drizzle.config.ts
+++ packages/drizzle/drizzle.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "drizzle-kit"
 import dotenv from "dotenv"
+console.log("dotenv.config()")
 dotenv.config({ path: "../../server.env" })
 const databaseUrl = process.env.DATABASE_URL!
 
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined. Make sure .env is loaded.")
+}
+
+console.log("process.env)", process.env)
+console.log("databaseUrl", databaseUrl)
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
