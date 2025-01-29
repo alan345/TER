88eafd135ae1e59569d1e2882682f912d36c5368
Alan <alan345@gmail.com>
2025-01-28 21:33:23 -0800

    test

--- server/index.ts
+++ server/index.ts
@@ -51,7 +51,10 @@ async function main() {
     })
 
     const port = Number(process.env.PORT) || 2022
-    await fastify.listen({ port })
+    await fastify.listen({
+      port,
+      host: "0.0.0.0",
+    })
     console.log("Server is running on port " + port)
   } catch (err) {
     fastify.log.error(err)
