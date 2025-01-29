fb162aadf9935e92327a8e73e0c63894e73e1a21
Alan <alan345@gmail.com>
2025-01-29 12:01:09 -0800

    remove drizzle from server

--- packages/drizzle/src/packageDrizzle.ts
+++ packages/drizzle/src/packageDrizzle.ts
@@ -1,8 +1,12 @@
-import * as drizzlePgCoreImport from "drizzle-orm/pg-core"
-export const drizzlePgCoreExport = drizzlePgCoreImport
+// import * as drizzlePgCoreImport from "drizzle-orm/pg-core"
+// export const drizzlePgCoreExport = drizzlePgCoreImport
 
-import * as drizzleNodePgImport from "drizzle-orm/node-postgres"
-export const drizzleOrm_NodePostgres = drizzleNodePgImport
+// export * as drizzleOrm_PgCore from "drizzle-orm/pg-core"
 
-import * as drizzleImport from "drizzle-orm"
-export const drizzleOrm = drizzleImport
+// import * as drizzleNodePgImport from "drizzle-orm/node-postgres"
+// export const drizzleOrm_NodePostgres = drizzleNodePgImport
+export * as drizzleOrm_NodePostgres from "drizzle-orm/node-postgres"
+
+// import * as drizzleImport from "drizzle-orm"
+// export const drizzleOrm = drizzleImport
+export * as drizzleOrm from "drizzle-orm"
